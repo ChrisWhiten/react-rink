7339db43976d54792d8f82625d750a91bd7887f8
Chris Whiten <chris.whiten@gmail.com>
2017-03-23 08:04:30 -0400

    Add basic day header

--- src/components/search/Timeline.js
+++ src/components/search/Timeline.js
@@ -13,40 +13,86 @@ class Timeline extends React.Component {
     };
   }
 
+  _generateTimelineRow(timelineItem) {
+    if (timelineItem.type === 'date') {
+      return (
+        <div key={`row-${timelineItem.id}`} className='timeline-row'>
+          <div key={timelineItem.id} className='timeline-header'>
+            <p>{timelineItem.value}</p>
+          </div>
+        </div>
+      );
+    } else {
+      // type === 'event'
+      return (
+        <div key={`row-${timelineItem.id}`} className='timeline-row'>
+          <div key={`icon-${timelineItem.id}`} className='event-icon'>
+            <Schedule className='event-icon-svg' />
+          </div>
+          <div key={timelineItem.id} className='timeline-item'>
+            <p>{timelineItem.value.datetime.toString()}</p>
+            <p>{timelineItem.value.host}</p>
+          </div>
+        </div>
+      );
+    }
+  }
+
   render() {
-    // TODO:
-    // - date splitter
+    const today = new Date();
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    let lastHour = today;
+    lastHour.setHours(lastHour.getHours() - 1);
+
     const events = [
       {
-        datetime: new Date(),
+        datetime: today,
         host: 'Chris',
         id: 'asbadfs',
       }, {
-        datetime: new Date(),
+        datetime: lastHour,
+        host: 'Jim',
+        id: 'ojiefw',
+      }, {
+        datetime: yesterday,
         host: 'Wayne',
         id: 'weofij',
       },
     ];
 
+    events.sort((a, b) => {
+      return a.datetime - b.datetime;
+    });
+
+    let timelineItems = [];
+    let currentDate = new Date(-8640000000000000) // earliest possible date :)
+    events.forEach(e => {
+      if (e.datetime.toDateString() !== currentDate.toDateString()) {
+        currentDate = e.datetime;
+        timelineItems.push({
+          type: 'date',
+          value: currentDate.toDateString(),
+          id: `date-${currentDate.toDateString()}`,
+        });
+      }
+
+      timelineItems.push({
+        type: 'event',
+        value: e,
+        id: `event-${e.id}`,
+      });
+    })
+
     return (
       <div className='timeline'>
         {
-          events.map(e => {
-            return (
-              <div key={`row-${e.id}`} className='timeline-row'>
-                <div key={`icon-${e.id}`} className='event-icon'>
-                    <Schedule className='event-icon-svg' />
-                </div>
-                <div key={e.id} className='timeline-item'>
-                  <p>{e.datetime.toString()}</p>
-                  <p>{e.host}</p>
-                </div>
-              </div>
-            )
+          timelineItems.map(e => {
+            return this._generateTimelineRow(e)
           })
         }
         {
-          events.length > 0 &&
+          timelineItems.length > 0 &&
           <div key='row-close' className='timeline-row'>
             <div key='icon-close' className='event-icon close'>
               <Schedule className='event-icon-svg' />
