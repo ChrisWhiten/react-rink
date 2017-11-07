bd54a8e9c2e2ee0a3fe03b69f3feab029071b90c
Chris Whiten <chris.whiten@gmail.com>
2017-11-07 07:55:05 -0500

    rig up date picker

--- src/components/search/FilterMenu.js
+++ src/components/search/FilterMenu.js
@@ -6,6 +6,7 @@ import EventIcon from 'material-ui/svg-icons/action/event';
 import Checkbox from 'material-ui/Checkbox';
 import classNames from 'classnames';
 import moment from 'moment';
+import {default as MaterialDatePicker} from 'material-ui/DatePicker';
 
 import './FilterMenu.css';
 
@@ -13,18 +14,31 @@ class FilterMenu extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  _handleDatePickerOpen() {
+    this.refs.bookingDatePicker.openDialog();
+  }
+
+  _handleDatePickerChange(nil, d) {
+    this.setState({
+      date: d,
+    });
   }
 
   render() {
-
     return (
       <div className='filter-menu'>
         <div className='filter-list-icon'>
           <FilterList className='filter-list-icon-svg' />
         </div>
-        <div className='filter-date-picker'>
+        <div className='filter-date-picker' onTouchTap={this._handleDatePickerOpen.bind(this)}>
           <span>
-            {moment(new Date()).format('ddd, MMM Do, YYYY')}
+            {moment(this.state.date).format('ddd, MMM Do, YYYY')}
           </span>
           <div className='filter-date-picker-icon'>
             <EventIcon className='filter-date-picker-svg' />
@@ -45,6 +59,16 @@ class FilterMenu extends React.Component {
           </div>
         </div>
 
+        <MaterialDatePicker
+          id='booking-date-picker'
+          style={{display: 'none'}}
+          ref='bookingDatePicker'
+          autoOk={true}
+          minDate={new Date()}
+          maxDate={new Date(2050, 0, 0, 0, 0, 0, 0)}
+          disableYearSelection={false}
+          onChange={this._handleDatePickerChange.bind(this)}
+        />
       </div>
     );
   }
